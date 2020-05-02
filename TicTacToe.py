7ca69933b448f0f3f68d20faf5641d8bccf12dae
Janaki <48669886+janvis04@users.noreply.github.com>
2020-05-02 17:54:51 +0200

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -17,14 +17,6 @@ def win(game):
         if row.count(row[0]) == len(row) and row[0] !=0:
             print(f"Player {row[0]} is the winner horizontally!")
             
-    # Vertical
-    for col in range(len(game)):
-        check = []
-        for row in game:
-            check.append(row[col])
-           
-        if check.count(check[0]) == len(check) and check[0] != 0:
-            print(f"Player {check[0]} is the winner vertically!")
             
     # Diagonal
     diags = []
